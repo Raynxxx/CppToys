2a11d6592c60f9b4014e3c7d1d638789d0bc0224
烧白君 <rayn1027@outlook.com>
2015-02-26 14:48:15 +0800

    Update String

--- MiniSTL/String.h
+++ MiniSTL/String.h
@@ -118,17 +118,55 @@ namespace rayn {
         ** @brief   Attempt to preallocate enough memory for specified size.
         */
         void reserve(size_type n = 0);
+        /*
+        ** @brief   Shrink memory to fit cur string length.
+        */
         void shrink_to_fit() {
             data_allocator::deallocate(_finish, _endOfStorage - _finish);
             _endOfStorage = _finish;
         }
 
         // Element access
+        /*
+        ** @brief   Access to the data in the string by index.
+        ** @param   pos The index of char to access.
+        ** @return  Read/Write reference to the character.
+        */
         reference operator[] (size_type pos) { return *(_start + pos); }
+        /*
+        ** @brief   Access to the data in the string by index.
+        ** @param   pos The index of char to access.
+        ** @return  Read-only reference to the character.
+        */
         const_reference operator[] (size_type pos) const { return *(_start + pos); }
+        /*
+        ** @brief   Access to the data in the string by index.
+        ** @param   pos The index of char to access.
+        ** @return  Read/Write reference to the character.
+        */
+        reference at(size_type pos) { return *(_start + pos); }
+        /*
+        ** @brief   Access to the data in the string by index.
+        ** @param   pos The index of char to access.
+        ** @return  Read-only reference to the character.
+        */
+        const_reference at(size_type pos) const { return *(_start + pos); }
+
+        /*
+        ** @return  Read/Write reference to the data at the first.
+        */
         reference front() { return *(_start); }
+        /*
+        ** @return  Read-only reference to the data at the first.
+        */
         const_reference front() const { return *(_start); }
+        /*
+        ** @return  Read/Write reference to the data at the last.
+        */
         reference back() { return *(_finish - 1); }
+        /*
+        ** @return  Read-only reference to the data at the last.
+        */
         const_reference back() const { return *(_finish - 1); }
 
         // Modifiers
@@ -155,8 +193,8 @@ namespace rayn {
         basic_string& append(const basic_string& str);
         /*
         ** @brief   Append n char from str starting at pos to this string.
-        ** @param   subpos  Index of the first character of str to append.
-        ** @param   sublen  The number of characters to append.
+        ** @param   subpos  Start Index in the inserted string.
+        ** @param   sublen  Length of characters to append.
         ** @return  Reference to this string.
         */
         basic_string& append(const basic_string& str, size_type subpos, size_type sublen = npos);
@@ -222,10 +260,155 @@ namespace rayn {
         */
         template <class InputIterator>
         iterator insert(iterator p, InputIterator first, InputIterator last);
-
+        /*
+        ** @brief   Insert value of string.
+        ** @param   pos The iterator refer to pos to insert.
+        ** @return  Reference to this string.
+        */
         basic_string& insert(size_type pos, const basic_string& str);
+        /*
+        ** @brief   Insert a substring.
+        ** @param   pos     Index in this string to insert.
+        ** @param   subpos  Start Index in the inserted string.
+        ** @param   sublen  Length of characters to insert.
+        ** @return  Reference to this string.
+        */
         basic_string& insert(size_type pos, const basic_string& str, size_type subpos, size_type sublen = npos);
+        /*
+        ** @brief   Insert a C-Style string.
+        ** @param   pos     Index in this string to insert.
+        ** @param   cstr    The C-string to insert.
+        ** @return  Reference to this string.
+        */
+        basic_string& insert(size_type pos, const CharT* cstr);
+        /*
+        ** @brief   Insert a C-Style substring.
+        ** @param   pos     Index in this string to insert.
+        ** @param   cstr    The C-string to insert.
+        ** @param   cstr    Number of characters to insert.
+        ** @return  Reference to this string.
+        */
+        basic_string& insert(size_type pos, const CharT* cstr, size_type n);
+        /*
+        ** @brief   Insert multiple characters.
+        ** @param   pos     Index in this string to insert.
+        ** @param   n       Number of characters to insert.
+        ** @param   ch      The value of Character to insert.
+        ** @return  Reference to this string.
+        */
+        basic_string& insert(size_type pos, size_type n, CharT ch);
 
+        /*
+        ** @brief   Remove characters.
+        ** @param   pos Start Index to remove.
+        ** @param   n   Number of char to remove. (DEFAULT = npos).
+        ** @return  Return the reference of this string.
+        */
+        basic_string& erase(size_type pos = 0, size_type n = npos);
+        /*
+        ** @brief   Remove characters.
+        ** @param   pos Iterator refer to The start index to remove. 
+        ** @return  Return the iterator refer to the location of first char removal.
+        */
+        iterator erase(iterator pos);
+        /*
+        ** @brief   Remove a range of characters [first, last). 
+        ** @return  Return the iterator refer to the end of the range.
+        */
+        iterator erase(iterator first, iterator last);
+        /*
+        ** @brief   Remove the last character.
+        */
+        void pop_back();
+
+        /*
+        ** @brief   Replace characters with value from another string.
+        ** @param   pos     Index of first char to replace.
+        ** @param   len     Number of char to be replaced.
+        ** @param   str     String to insert.
+        ** @return  Reference to this string.
+        */
+        basic_string& replace(size_type pos, size_type len, const basic_string& str);
+        /*
+        ** @brief   Replace characters from another substring.
+        ** @param   pos     Index of first char to replace.
+        ** @param   len     Number of char to be replaced.
+        ** @param   str     String to insert.
+        ** @param   subpos  Index of first char of str to use.
+        ** @param   sublen  Number of char from str to use.
+        ** @return  Reference to this string.
+        */
+        basic_string& replace(size_type pos, size_type len, const basic_string& str,
+            size_type subpos, size_type sublen = npos);
+        /*
+        ** @brief   Replace characters from C-Style string.
+        ** @param   pos     Index of first char to replace.
+        ** @param   len     Number of char to be replaced.
+        ** @param   cstr    C-String to insert.
+        ** @param   len2    Number of char from cstr to use.
+        ** @return  Reference to this string.
+        */
+        basic_string& replace(size_type pos, size_type len, const CharT* cstr, size_type len2);
+        /*
+        ** @brief   Replace characters from C-Style string.
+        ** @param   pos     Index of first char to replace.
+        ** @param   len     Number of char to be replaced.
+        ** @param   cstr    C-String to insert.
+        ** @return  Reference to this string.
+        */
+        basic_string& replace(size_type pos, size_type len, const CharT* cstr);
+        /*
+        ** @brief   Replace characters from C-Style string.
+        ** @param   pos     Index of first char to replace.
+        ** @param   len     Number of char to be replaced.
+        ** @param   len2    Number of char to insert.
+        ** @param   ch      Characters to insert.
+        ** @return  Reference to this string.
+        */
+        basic_string& replace(size_type pos, size_type len, size_type len2, CharT ch);
+        /*
+        ** @brief   Replace range of characters with string.
+        ** @param   i1  Iterator refer to start of range to replace.
+        ** @param   i2  Iterator refer to end of range to replace.
+        ** @return  Reference to this string.
+        */
+        basic_string& replace(iterator i1, iterator i2, const basic_string& str);
+        /*
+        ** @brief   Replace range of characters with C-Style string.
+        ** @param   i1      Iterator refer to start of range to replace.
+        ** @param   i2      Iterator refer to end of range to replace.
+        ** @param   cstr    C-String value to insert.
+        ** @param   n       Number of chars from cstr to insert.
+        ** @return  Reference to this string.
+        */
+        basic_string& replace(iterator i1, iterator i2, const CharT* cstr, size_type n);
+        /*
+        ** @brief   Replace range of characters with C-Style string.
+        ** @param   i1      Iterator refer to start of range to replace.
+        ** @param   i2      Iterator refer to end of range to replace.
+        ** @param   cstr    C-String value to insert.
+        ** @return  Reference to this string.
+        */
+        basic_string& replace(iterator i1, iterator i2, const CharT* cstr);
+        /*
+        ** @brief   Replace range of characters with C-Style string.
+        ** @param   i1      Iterator refer to start of range to replace.
+        ** @param   i2      Iterator refer to end of range to replace.
+        ** @param   n       Number of chars to insert.
+        ** @param   ch      Character to insert.
+        ** @return  Reference to this string.
+        */
+        basic_string& replace(iterator i1, iterator i2, size_type n, CharT ch);
+        /*
+        ** @brief   Replace range of characters with C-Style string.
+        ** @param   i1      Iterator refer to start of range to replace.
+        ** @param   i2      Iterator refer to end of range to replace.
+        ** @param   first   Iterator refer to start of range to insert.
+        ** @param   last    Iterator refer to end of range to insert.
+        ** @return  Reference to this string.
+        */
+        template <class InputIterator>
+        basic_string& replace(iterator i1, iterator i2, InputIterator first, InputIterator last);
     };
 
     typedef basic_string<char>      string;
