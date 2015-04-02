5818eac2c26552736e8d4ff363368e7a85d19779
烧白君 <rayn1027@outlook.com>
2015-04-02 13:29:52 +0800

    Add API of rayn::list

--- MiniSTL/List.h
+++ MiniSTL/List.h
@@ -31,7 +31,7 @@ namespace rayn {
 
     // ÁÐ±íµü´úÆ÷
     template <class T>
-    struct __list_iterator : public bidirectional_iterator <T, ptrdiff_t> {
+    struct __list_iterator : public bidirectional_iterator<T, ptrdiff_t> {
         typedef __list_iterator<T>  iterator;
         typedef __list_iterator<T>  self;
 
@@ -75,32 +75,99 @@ namespace rayn {
     template <class T>
     class list {
     protected:
-        typedef __list_node<T>                  list_node;
-        typedef allocator<list_node>            node_allocator;
-        typedef list_node*                      link_type;
+        typedef __list_node<T>                      list_node;
+        typedef allocator<list_node>                node_allocator;
+        typedef list_node*                          link_type;
     public:
-        typedef T                               value_type;
-        typedef T&                              reference;
-        typedef __list_iterator<T>              iterator;
-        typedef reverse_iterator_t<iterator>    reverse_iterator;
-        typedef size_t                          size_type;
+        typedef T                                   value_type;
+        typedef T&                                  reference;
+        typedef const T&                            const_reference;
+        typedef __list_iterator<T>                  iterator;
+        typedef __list_iterator<const T>            const_iterator;
+        typedef reverse_iterator_t<iterator>        reverse_iterator;
+        typedef reverse_iterator_t<const_iterator>  const_reverse_iterator;
+        typedef size_t                              size_type;
+        typedef ptrdiff_t                           difference_type;
 
     protected:
         link_type node;
 
     public:
+        // Default Constructor
         list() {
-            node = get_node();
-            node->next = node;
-            node->prev = node;
+            empty_initialize();
         }
+        explicit list(size_type count);
+        list(size_type count, const T& value);
+        template <class InputIterator>
+        list(InputIterator first, InputIterator last);
+        list(const list& other);
+        list(list&& other);
+        ~list();
 
+        list& operator== (const list& other);
+        list& operator== (list&& other);
+
+        void assign(size_type count, const T& value);
+        template <class InputIterator>
+        void assign(InputIterator first, InputIterator last);
+
+        reference front();
+        const_reference front() const;
+        reference back();
+        const_reference back() const;
 
         iterator begin() { return node->next; }
+        const_iterator begin() const;
+        const_iterator cbegin() const;
+
         iterator end() { return node; }
+        const_iterator end() const;
+        const_iterator cend() const;
+
+        reverse_iterator rbegin();
+        const_reverse_iterator rbegin() const;
+        const_reverse_iterator crbegin() const;
+
+        reverse_iterator rend();
+        const_reverse_iterator rend() const;
+        const_reverse_iterator crend() const;
+
         bool empty() const {
             return node->next == node;
         }
+        size_type size() const;
+        
+        void clear();
+        iterator insert(iterator position, const T& value);
+        iterator insert(const_iterator position, const T& value);
+        iterator insert(const_iterator position, T&& value);
+        iterator erase(iterator position);
+        void push_back(const T& x);
+        void pop_back();
+        void push_front(const T& x);
+        void pop_front();
+        void resize();
+        void swap();
+        
+        void merge(list& x);
+        void spiice(iterator position, list& x);
+        void spiice(iterator position, list& x, iterator i);
+        void spiice(iterator position, list& x, iterator first, iterator last);
+        void remove(const T& value);
+        void remove_if();
+        void reverse();
+        void unique();
+        void sort();
+
+    public:
+        friend bool operator== (list& lhs, list& rhs);
+        friend bool operator!= (list& lhs, list& rhs);
+        friend bool operator< (list& lhs, list& rhs);
+        friend bool operator<= (list& lhs, list& rhs);
+        friend bool operator> (list& lhs, list& rhs);
+        friend bool operator>= (list& lhs, list& rhs);
+        friend void swap(list& lhs, list& rhs);
 
     protected:
         link_type get_node() {
@@ -118,6 +185,15 @@ namespace rayn {
             rayn::destroy(&(p->data));
             put_node(p);
         }
+
+        void empty_initialize() {
+            node = get_node();
+            node->next = node;
+            node->prev = node;
+        }
+
+        void transfer(iterator position, iterator first, iterator last);
+
     };
 
 }
